907539d1a26c1e7c76e6b0504c3994280fb2621b
徐自立 <1547170390@qq.com>
2024-09-20 11:29:07 +0800

    Springboot整合rabbitmq

--- springboot-template/src/test/java/com/zl/template/SpringbootTemplateApplicationTests.java
+++ springboot-template/src/test/java/com/zl/template/SpringbootTemplateApplicationTests.java
@@ -1,6 +1,7 @@
 package com.zl.template;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,9 +12,16 @@ class SpringbootTemplateApplicationTests {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Test
     void RedisTest() {
         stringRedisTemplate.opsForValue().set("hello", "world");
+    }
+    @Test
+    void RabbitMqTest(){
+        rabbitTemplate.convertAndSend("test", "world");
     }
 
 }
