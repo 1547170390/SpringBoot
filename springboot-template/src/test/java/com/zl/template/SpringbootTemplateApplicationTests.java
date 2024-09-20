fb57b4ad9e7192000f74224cdc2ce62608ffa358
徐自立 <1547170390@qq.com>
2024-09-20 10:51:14 +0800

    Springboot整合redis

--- springboot-template/src/test/java/com/zl/template/SpringbootTemplateApplicationTests.java
+++ springboot-template/src/test/java/com/zl/template/SpringbootTemplateApplicationTests.java
@@ -1,13 +1,19 @@
 package com.zl.template;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class SpringbootTemplateApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
-    void contextLoads() {
+    void RedisTest() {
+        stringRedisTemplate.opsForValue().set("hello", "world");
     }
 
 }
